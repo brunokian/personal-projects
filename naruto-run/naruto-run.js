3db5edeec081cfc2b62a9707249d2fee137ce371
brunokian <brunokian@hotmail.com>
2022-04-09 02:15:35 -0300

    corrida automatica

--- naruto-run/naruto-run.js
+++ naruto-run/naruto-run.js
@@ -5,15 +5,14 @@ let meioDoCampo = (window.innerWidth * 0.5) - 20
 let fimDoCampo = window.innerWidth - 101
 const startButton = document.getElementById('start-button')
 
-
 naruto.style.marginLeft = '1px'
 sasuke.style.marginLeft = `${fimDoCampo}px`
 trofeu.style.marginLeft = `${meioDoCampo}px`
 
 function startRunning() {
-    naruto.style.marginLeft = parseInt(naruto.style.marginLeft) + Math.random()*50 + 'px'
+    naruto.style.marginLeft = parseInt(naruto.style.marginLeft) + Math.random()*20 + 'px'
     console.log(parseInt(naruto.style.marginLeft))
-    sasuke.style.marginLeft = parseInt(sasuke.style.marginLeft) - Math.random()*50 + 'px'
+    sasuke.style.marginLeft = parseInt(sasuke.style.marginLeft) - Math.random()*20 + 'px'
 
     if(parseInt(naruto.style.marginLeft) > (window.innerWidth / 2) - 80) {
         alert ('NARUTO É O VERCEDOR!!!')
@@ -29,11 +28,15 @@ function startRunning() {
 function resetGame() {
     naruto.style.marginLeft = '1px'
     sasuke.style.marginLeft = `${fimDoCampo}px`
+    clearInterval(run)
 }
 
-
+function running() {
+    setInterval(startRunning, 50)
+}
     
  
 
-startButton.addEventListener('click', startRunning)
+startButton.addEventListener('click',running)
+// setInterval(startRunning, 500)
 
